0d0d12584dea4d576df70f772b5f9c38cdcb5d39
mateoBodiniARG <bodinidev@gmail.com>
2024-04-12 20:06:30 -0300

    feat: Inicilizacion LumarV2

---
+++ src/app/layout.js
@@ -0,0 +1,54 @@
+//= Global Styles
+import "@/styles/main.scss"
+import "@/styles/preloader.css";
+import "@/styles/modal-video.css";
+import "swiper/css/bundle";
+import "react-circular-progressbar/dist/styles.css";
+import Cursor from "@/components/Common/Cursor";
+import ProgressScroll from "@/components/Common/ProgressScroll";
+import Script from "next/script";
+import generateStylesheetObject from "@/common/generateStylesheetsObject";
+
+export const metadata = {
+  title: "Vie",
+  description: "Vie - Multi-Purpose React.js Next.js Template",
+  keywords: ["HTML5", "Template", "Vie", "Multi-Purpose", "themeforest"],
+  icons: {
+    icon: "/assets/imgs/favicon.ico",
+    shortcut: "/assets/imgs/favicon.ico",
+  },
+  other: generateStylesheetObject([
+    "https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap",
+    "https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@100;200;300;400;500;600;700;800;900&display=swap",
+    "https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap",
+    "/css/dark.css",
+  ]),
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Cursor />
+        <ProgressScroll />
+
+        {children}
+        <Script strategy="beforeInteractive" src="/js/wow.min.js"></Script>
+        <Script
+          strategy="beforeInteractive"
+          src="/js/splitting.min.js"
+        ></Script>
+        <Script
+          strategy="beforeInteractive"
+          src="/js/simpleParallax.min.js"
+        ></Script>
+        <Script
+          strategy="beforeInteractive"
+          src="/js/isotope.pkgd.min.js"
+        ></Script>
+        <Script strategy="lazyOnload" src="/js/pace.min.js"></Script>
+        <Script strategy="lazyOnload" src="/js/wowInit.js"></Script>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,49 @@
+//= Scripts
+import generateStylesheetObject from "@/common/generateStylesheetsObject";
+
+//= Page Components
+import Loading from "@/components/Common/Loader";
+import Navbar from "@/components/Common/Navbar";
+import Header from "@/components/Headers/Header4";
+import AboutUs from "@/components/AboutUs/AboutUs3";
+import Works from "@/components/Works/Works3";
+import Services from "@/components/Services/Services3";
+import MinimalArea2 from "@/components/Others/MinimalArea2";
+import FullTestimonials from "@/components/Testimonials/FullTestimonials";
+import Team from "@/components/Team/Team1";
+import Blogs from "@/components/Blogs/Blogs4";
+import CallToAction from "@/components/Others/CallToAction";
+import Footer from "@/components/Common/Footer";
+
+export const metadata = {
+  title: "Vie - Preview",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    other: generateStylesheetObject([
+      "https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap",
+      "https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@100;200;300;400;500;600;700;800;900&display=swap",
+      "https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap",
+      "/css/dark.css",
+    ]),
+  },
+};
+
+export default function LandingPreview() {
+  return (
+    <>
+      <Loading />
+      <Navbar />
+      <Header />
+      <AboutUs />
+      <Works />
+      <Services />
+      <MinimalArea2 />
+      <FullTestimonials classText="pb-0" />
+      <Team />
+      <Blogs />
+      <CallToAction />
+      <Footer />
+    </>
+  );
+}
